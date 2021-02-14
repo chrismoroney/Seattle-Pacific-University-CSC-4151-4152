21c5907412f553e7572e44ce72757b629fdbf922
andrewwmclain <mclaina1@spu.edu>
2021-02-13 22:40:04 -0800

    added user model and some routing

--- database.js
+++ database.js
@@ -1,28 +1,52 @@
-const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
+//
+// async function main(){
+//     const uri = "mongodb+srv://andrewwmclainCSC415n:75@vNNhYbP9XmaV@cluster0.uueiv.mongodb.net/Users?retryWrites=true&w=majority";
+//
+//
+//     const client = new MongoClient(uri, {useUnifiedTopology: true});
+//
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
+//
+//         // Make the appropriate DB calls
+//         // await  listDatabases(client);
+//
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+// main().catch(console.error);
 
-async function main(){
-    const uri = "mongodb+srv://andrewwmclainCSC415n:75@vNNhYbP9XmaV@cluster0.uueiv.mongodb.net/Users?retryWrites=true&w=majority";
-
-
-    const client = new MongoClient(uri, {useUnifiedTopology: true});
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-
-        // Make the appropriate DB calls
-        // await  listDatabases(client);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-main().catch(console.error);
 //list databases to test connection
 // async function listDatabases(client){
 //     databasesList = await client.db().admin().listDatabases();
 //     console.log("Databases:");
 //     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 // };
+
+const mongoose = require('mongoose');
+
+// Update with your own Database URI
+// const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://andrewwmclain:cluster0password@cluster0.fyztf.mongodb.net/books?retryWrites=true&w=majority\n';
+
+const mongoDB = process.env.MONGODB_URI || "mongodb+srv://andrewwmclainCSC415n:75@vNNhYbP9XmaV@cluster0.uueiv.mongodb.net/Users?retryWrites=true&w=majority";
+
+mongoose
+    .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('DB Connected!');
+    })
+    .catch(error => {
+        console.log('Connection Error: ${err.message}');
+    });
+
+const db = mongoose.connection;
+
+// Bind the console to errors, to show them on console
+db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
+
+module.exports = db;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,177 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+//
+// module.exports = router;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// For the routes
+let express = require('express');
+let router = express.Router();
+// For the Data Model
+let UserSchema = require('../models/User.js');
+
+
+function HandleError(response, reason, message, code){
+  console.log('ERROR: ' + reason);
+  response.status(code || 500).json({"error:": message});
+}
+
+router.post('/', (request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  let obj = JSON.parse(JSON.stringify(request.body));
+  let newUser = obj;
+  // console.log(newBook);
+
+  // if (!newUser.Name || !newUser.Author || !newUser.ISBN || !newUser.Price){
+  //   HandleError(response, 'Missing Info', 'Form data missing', 500);
+  // }else{
+  //   var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+  //   var match = re.exec(newBook.ISBN);
+
+    // if(!match){
+    //   HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+    // }else{
+      let user = new UserSchema({
+        Name: newUser.Name,
+        Password: newUser.Password
+      });
+      user.save((error) => {
+        if (error){
+          response.send({"error": error});
+        }else{
+          response.send({"id": user.Name});
+        }
+      });
+    // }
+  // }
 });
+
+router.get('/', (request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  // let author = request.query['author'];
+  // if (author){
+  //   UserSchema
+  //       .find({"Author": author})
+  //       .exec( (error, books) => {
+  //         if (error){
+  //           response.send({"error": error});
+  //         }else{
+  //           response.send(books);
+  //         }
+  //       });
+  // }else{
+  //   UserSchema
+  //       .find()
+  //       .exec( (error, books) => {
+  //         if (error){
+  //           response.send({"error": error});
+  //         }else{
+  //           response.send(books);
+  //         }
+  //       });
+  // }
+} );
+
+// router.get('/:isbn', (request, response, next) =>{
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//
+//   var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+//   var match = re.exec(request.params.isbn);
+//
+//   if(!match){
+//     HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+//   }else{
+//     UserSchema
+//         .find({"ISBN": request.params.isbn}, (error, result) =>{
+//           if (error) {
+//             response.status(500).send(error);
+//           }
+//           if (result && result.length > 0){
+//             response.send(result);
+//           }else{
+//             response.status(404).send({"isbn": request.params.isbn, "error":  "Not Found"});
+//           }
+//         });
+//   }
+// });
+//
+//
+// router.patch('/:isbn', (request, response, next) =>{
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//
+//   var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+//   var match = re.exec(request.params.isbn);
+//
+//   if(!match){
+//     HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+//   }else {
+//     UserSchema
+//         .findOne({"ISBN": request.params.isbn}, (error, result) => {
+//           if (error) {
+//             response.status(500).send(error);
+//             // console.log("test error");
+//           } else if (result) {
+//             if (request.body.isbn) {
+//               delete request.body.isbn;
+//             }
+//             for (let field in request.body) {
+//               if(!request.body[field] || request.body[field].length == 0){
+//                 HandleError(response, 'Missing field', 'One or more fields is missing', 500);
+//               }else{
+//                 result[field] = request.body[field];
+//               }
+//             }
+//             result.save((error, friend) => {
+//               if (error) {
+//                 response.status(500).send(error);
+//               }
+//               response.send(friend);
+//             });
+//           } else {
+//             response.status(404).send({"ISBN": request.params.isbn, "error": "Not Found"});
+//           }
+//
+//         });
+//   }
+// });
+
+// router.delete('/:isbn', (request, response, next) =>{
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//
+//   var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+//   var match = re.exec(request.params.isbn);
+//
+//   if(!match){
+//     HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+//   }else {
+//     UserSchema
+//         .findOne({"ISBN": request.params.isbn}, (error, result) => {
+//           if (error) {
+//             response.status(500).send(error);
+//           } else if (result) {
+//             result.remove((error) => {
+//               if (error) {
+//                 response.status(500).send(error);
+//               }
+//               response.send({"deletedISBN": request.params.isbn});
+//             });
+//           } else {
+//             response.status(404).send({"ISBN": request.params.isbn, "error": "Not Found"});
+//           }
+//         });
+//   }
+// });
 
 module.exports = router;
