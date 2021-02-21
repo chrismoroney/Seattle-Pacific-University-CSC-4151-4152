1f1ab0020a13ba11668aee866e7b13e6faa36457
andrewwmclain <mclaina1@spu.edu>
2021-02-20 20:47:59 -0800

    script changes

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var myprofileRouter = require('./routes/myprofile');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
