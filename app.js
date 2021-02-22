0fcfb30f73c368808d5722544ee186f45f621b60
Christopher Moroney <christopher.moroney1@gmail.com>
2021-02-22 13:15:08 -0800

    initial create profile page

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var messagesRouter = require('./routes/messages');
 var postsRouter = require('./routes/posts');
 var homepageRouter = require('./routes/homepage');
 var myprofileRouter = require('./routes/myprofile');
+var createprofileRouter = require('./routes/createprofile');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,6 +39,7 @@ app.use('/messages', messagesRouter);
 app.use('/posts', postsRouter);
 app.use('/homepage', homepageRouter);
 app.use('/myprofile', myprofileRouter);
+app.use('/createprofile', createprofileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
