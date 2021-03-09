5977c78f2ee445fc6b0940a5926cc49093facc73
andrewwmclain <mclaina1@spu.edu>
2021-03-09 14:09:10 -0800

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { auth } = require('express-openid-connect');
 
 app.use(
     auth({
-      authRequired: false,
+      authRequired: true,
       auth0Logout: true,
       issuerBaseURL: process.env.ISSUER_BASE_URL,
       baseURL: process.env.BASE_URL,
