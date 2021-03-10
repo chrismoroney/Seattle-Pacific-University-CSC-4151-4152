6b5271a1b223057a58086ee339a881ce7266e620
andrewwmclain <mclaina1@spu.edu>
2021-03-09 19:38:56 -0800

    messenger stuff

--- public/javascripts/messenger.js
+++ public/javascripts/messenger.js
@@ -3,12 +3,43 @@ var messages = document.getElementById("messages");
 var username = document.getElementById('username').innerText;
 
 
+var url = 'https://lingojiveapi.herokuapp.com/messages';
+// var url = 'http://localhost:5000/posts';
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        // messages.innerHTML = '';
+        var response = JSON.parse(this.responseText);
+        for(let i = 0; i < response.length; ++i){
+            let li = document.createElement("li");
+            let span = document.createElement("span");
+            messages.appendChild(li).append(response[i].Message);
+            messages.appendChild(span).append(response[i].Name);
+
+            // var item = document.createElement('li');
+            // item.textContent = response[i].Body;
+            // // messages.appendChild(item);
+            // messages.prepend(item);
+            // var user = document.createElement('div');
+            // user.textContent = response[i].Name;
+            // item.prepend(user);
+        }
+    }
+};
+
+xhttp.open("GET", url,
+    true);
+xhttp.send();
+
+
 (function() {
     $("form").submit(function(e) {
         let li = document.createElement("li");
         var message = document.getElementById('message').innerText;
         // var message = $("#message").val();
-
         e.preventDefault(); // prevents page reloading
         // socket.emit("chat message", $("#message").val());
         // username = document.getElementById('username').innerText;
@@ -34,6 +65,8 @@ var username = document.getElementById('username').innerText;
             true);
         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhttp.send(params);
+
+        document.getElementById('message').value = '';
 
         return false;
     });
