9da60dec522bb5924397290d02a9902b0e975a7c
Chris <christopher.moroney1@gmail.com>
2021-03-25 15:15:14 -0700

    finish up existing edit profile api

--- public/javascripts/editprofile.js
+++ public/javascripts/editprofile.js
@@ -55,21 +55,26 @@ document.getElementById("btnUpdateUser").addEventListener("click", (event) =>{
     let lastname = document.getElementById("lastname").value;
     let password = document.getElementById("password").value;
     let confirmpassword = document.getElementById("confirm_password").value;
-    let url = "https://lingojiveapi.herokuapp.com/users/" + username;
+    let url = "https://lingojiveapi.herokuapp.com/users/" + username + "/";
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function (){
         if (this.readyState == 4 && this.status == 200){
-            if (name === "" && lastname === "" && password === ""){
+            if (firstname === "" && lastname === "" && password === "" && confirmpassword === ""){
                 document.getElementById("output2").innerHTML =
                     "<pre>" + "Please fill in at least one field above" + "</pre>";
-            } else {
+            } else if (username === "" || this.responseText === "[]"){
                 document.getElementById("output2").innerHTML =
-                    CreateTable(JSON.parse(this.response)) + "<p></p>" +
-                    "<pre>" + "Successfully updated user to the catalog!" + "</pre>";
+                    "<pre>" + "Input a valid username above first (use the search button to find a valid username)" + "</pre>";
+            }
+            else {
+                let apiResponse = "[" + this.responseText + "]";
+                document.getElementById("output2").innerHTML =
+                    CreateTable(JSON.parse(apiResponse)) + "<p></p>" +
+                    "<pre>" + "Successfully saved new user info. " + "</pre>";
             }
         }
     };
-    if (password == confirmpassword){
+    if (password === confirmpassword){
         if (firstname !== ""){
             if (lastname !== "" || password !== "" || confirmpassword !== "") {
                 firstname = "firstname=" + document.getElementById("firstname").value + "&";
@@ -92,14 +97,19 @@ document.getElementById("btnUpdateUser").addEventListener("click", (event) =>{
             }
         }
         if (confirmpassword !== ""){
-            confirmpassword = "confirmpassword=" + document.getElementById("confirmpassword").value + "&";
+            confirmpassword = "confirmpassword=" + document.getElementById("confirm_password").value;
         }
     } else {
-
+        document.getElementById("output2").innerHTML =
+            "<pre>" + "Your passwords do not match. Please try again. " + "</pre>";
     }
     let userData = firstname + lastname + password + confirmpassword;
     xhttp.open('PATCH', url, true);
     // Just needed to place this line AFTER opening the object
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.send(userData);
+});
+
+document.getElementById("btnDiscardChanges").addEventListener("click", (event) =>{
+    window.location.pathname = "myprofile";
 });
