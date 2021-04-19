59b54051f27e80b700b47d61e8d9c225ad044bb3
andrewwmclain <mclaina1@spu.edu>
2021-04-18 19:45:33 -0700

    hard coded user props

--- routes/otherprofile.js
+++ routes/otherprofile.js
@@ -4,7 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     // res.render('index', { title: 'Express' });
-    res.render('otherprofile.html', {root: 'views', username: req.session.username});
+    res.render('otherprofile.html', {
+        root: 'views', username: "test username", firstName: "test firstname",
+        lastName: "test lastname", bio: "test bio", langExp: "test langExp",
+        langLearn: "test langLearn"})
 });
 
 module.exports = router;
