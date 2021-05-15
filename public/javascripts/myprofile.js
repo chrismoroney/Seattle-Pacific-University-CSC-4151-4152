8588baa014184f19e7ce877587863ccf07a6719f
Chris <christopher.moroney1@gmail.com>
2021-05-15 14:56:42 -0700

    Can retrieve profile pic from other profiles, formatting

--- public/javascripts/myprofile.js
+++ public/javascripts/myprofile.js
@@ -24,3 +24,28 @@ xhttp.onreadystatechange = function(){
 
 xhttp.open("GET", url, true);
 xhttp.send();
+
+function checkProfilePic(){
+    let url = "http://lingojiveapi.herokuapp.com/users/" + username;
+    let xml = new XMLHttpRequest();
+    xml.onload = function (){
+        if (this.readyState == 4 && this.status == 200){
+            checkImage(JSON.parse(this.responseText))
+        };
+    };
+    xml.open('GET', url,true);
+    xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xml.send();
+}
+
+function checkImage(users){
+    for (let user in users) {
+        let img = users[user]["profileImage"];
+        if(img != ""){
+            document.getElementById("photo").src = "http://lingojiveapi.herokuapp.com/" + img;
+        } else {
+            document.getElementById("photo").src = "../Picture/profilephoto.png";
+        }
+    }
+}
+window.onload=checkProfilePic;
